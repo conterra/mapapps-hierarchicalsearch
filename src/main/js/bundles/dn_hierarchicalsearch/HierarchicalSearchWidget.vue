84e28d2628fd2410efd824200e6314c54446e3b0
SarahLechler <s_lech05@uni-muenster.de>
2019-02-27 11:11:22 +0100

    Update bundle to mapapps version 4.6.0

--- src/main/js/bundles/dn_hierarchicalsearch/HierarchicalSearchWidget.vue
+++ src/main/js/bundles/dn_hierarchicalsearch/HierarchicalSearchWidget.vue
@@ -2,21 +2,22 @@
     <div class="hierarchicalsearch">
         <v-progress-linear v-if="loading" :active="loading" :indeterminate="true"
                            class="pa-0 ma-0"></v-progress-linear>
-        <v-container fluid grid-list-md fill-height>
+        <v-container class="pa-1" fluid grid-list-md fill-height>
             <v-layout>
                 <v-flex v-for="(field, index) in fields" :key="index">
-                    <v-select
+                    <v-combobox
                         v-model="field.value"
                         :items="field.items"
                         :label="field.label"
                         :disabled="field.disabled"
                         :loading="field.loading"
                         v-on:input="$emit('selected', field, index)"
-                        combobox
                         outline
                         clearable="true"
+                        full-width="true"
                         open-on-clear="true"
-                    ></v-select>
+                        hide-details="true"
+                    ></v-combobox>
                 </v-flex>
             </v-layout>
         </v-container>
