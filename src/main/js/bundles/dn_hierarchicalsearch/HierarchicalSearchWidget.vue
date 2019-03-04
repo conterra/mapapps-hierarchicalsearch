ce96152cc4960e28c79ac85f25ff3976634fc39c
mste <m.stein@conterra.de>
2019-03-04 08:54:03 +0100

    Add eslintrc file and reformat code

--- src/main/js/bundles/dn_hierarchicalsearch/HierarchicalSearchWidget.vue
+++ src/main/js/bundles/dn_hierarchicalsearch/HierarchicalSearchWidget.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="hierarchicalsearch">
-        <v-progress-linear v-if="loading" :active="loading" :indeterminate="true"
-                           class="pa-0 ma-0"></v-progress-linear>
-        <v-container class="pa-1" fluid grid-list-md fill-height>
+        <v-progress-linear
+            v-if="loading"
+            :active="loading"
+            :indeterminate="true"
+            class="pa-0 ma-0"/>
+        <v-container
+            class="pa-1"
+            fluid
+            grid-list-md
+            fill-height>
             <v-layout>
-                <v-flex v-for="(field, index) in fields" :key="index">
+                <v-flex
+                    v-for="(field, index) in fields"
+                    :key="index">
                     <v-combobox
                         v-model="field.value"
                         :items="field.items"
                         :label="field.label"
                         :disabled="field.disabled"
                         :loading="field.loading"
-                        v-on:input="$emit('selected', field, index)"
-                        outline
-                        clearable="true"
-                        full-width="true"
-                        open-on-clear="true"
-                        hide-details="true"
-                    ></v-combobox>
+                        clearable
+                        full-width
+                        open-on-clear
+                        hide-details
+                        @input="$emit('selected', field, index)"
+                    />
                 </v-flex>
             </v-layout>
         </v-container>
