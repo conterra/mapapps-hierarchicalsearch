8729c3a3251977afebc46828499552c691ac029b
s.holtkamp <s.holtkamp@conterra.de>
2023-06-06 15:40:40 +0200

    Add warning texts

--- src/main/js/bundles/dn_hierarchicalsearch/nls/bundle.ts
+++ src/main/js/bundles/dn_hierarchicalsearch/nls/bundle.ts
@@ -1,18 +1,18 @@
-/*
- * Copyright (C) 2022 con terra GmbH (info@conterra.de)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+///
+/// Copyright (C) 2022 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
 
 module.exports = {
     root: ({
@@ -25,7 +25,9 @@ module.exports = {
         noResultsError: "No results found for your query!",
         ui: {
             search: "Search",
-            resultUiTitle: "Hierarchical Search Result"
+            resultUiTitle: "Hierarchical Search Result",
+            resultUiWarning: "Result cannot be shown in result-ui as it is not configured for the app.",
+            resultcenterWarning: "Result cannot be shown in resultcenter as it is not configured for the app."
         }
     }),
     "de": true
