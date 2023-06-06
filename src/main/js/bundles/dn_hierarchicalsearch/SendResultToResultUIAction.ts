8f983a8023a2c652edcccc994cbf4c3b92332121
s.holtkamp <s.holtkamp@conterra.de>
2023-06-06 15:41:18 +0200

    Rework SendResultToResultUIAction as a map-action

--- src/main/js/bundles/dn_hierarchicalsearch/SendResultToResultUIAction.ts
+++ src/main/js/bundles/dn_hierarchicalsearch/SendResultToResultUIAction.ts
@@ -14,41 +14,45 @@
 /// limitations under the License.
 ///
 
-/*
- * Copyright (C) 2022 con terra GmbH (info@conterra.de)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-import ComplexQuery from "@conterra/ct-mapapps-typings/store-api/ComplexQuery";
 import type { ResultViewerService } from "result-api/api";
 
 export default class SendResultToResultUIAction {
 
+    public id: string;
+    public immediate: Boolean;
+
     private resultViewerService: ResultViewerService;
     private _i18n: any;
 
-    async executeAction(store: any, query: ComplexQuery) {
-        const dataTableFactory = this.resultViewerService.dataTableFactory;
-        const title = this._i18n.get().ui.resultUiTitle;
+    constructor() {
+        // unique ID of the action
+        this.id = "sendResultToResultUI";
+        // this is a sequential action
+        this.immediate = false;
+    }
 
-        const dataTable = await dataTableFactory.createDataTableFromStoreAndQuery({
-            dataTableTitle: title,
-            dataSource: store,
-            queryExpression: query,
-            queryOptions: {}
-        });
+    // trigger method which is called with the search result items
+    async trigger(options: any) {
+        if (this.resultViewerService) {
+            const store = options.source;
+            const query = options["query"];
 
-        const dataTableCollection = dataTableFactory.createDataTableCollection([dataTable]);
-        this.resultViewerService.open(dataTableCollection);
+            const dataTableFactory = this.resultViewerService.dataTableFactory;
+            const title = this._i18n.get().ui.resultUiTitle;
+
+            const dataTable = await dataTableFactory.createDataTableFromStoreAndQuery({
+                dataTableTitle: title,
+                dataSource: store,
+                queryExpression: query,
+                queryOptions: {}
+            });
+
+            const dataTableCollection = dataTableFactory.createDataTableCollection([dataTable]);
+            this.resultViewerService.open(dataTableCollection);
+        }
+        else {
+            const warning = this._i18n.get().ui.resultUiWarning;
+            console.warn(warning)
         }
     }
+}
